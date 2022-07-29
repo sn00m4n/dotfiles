7b0b24e0f2ce26c92083f2aa02fb02b4c8c4d347
myOmikron <git@omikron.dev>
2022-07-29 08:55:01 +0200

    Added more packages and locales

--- installation.sh
+++ installation.sh
@@ -20,14 +20,26 @@ cargo install wasm-pack
 # Java
 yay -Sy --needed gradle gradle-doc gradle-src
 
+# D
+yay -Sy --needed dlang
+
 # Libraries
-yay -Sy --needed fmt qt5-imageformats kimageformats kamera noto-fonts-cjk
+yay -Sy --needed fmt qt5-imageformats kimageformats kamera noto-fonts-cjk 
+
+# Screen capture
+yay -Sy --needed xdg-desktop-portal xdg-desktop-portal-kde obs-studio
+
+# Canon webcam setup
+yay -Sy --needed gphoto2  
 
 # CLI tools
-yay -Sy --needed fish neovim neovim-plug tor rsync man thefuck openconnect texlive-most xorg-fonts ssss screen nginx
+yay -Sy --needed fish neovim neovim-plug tor rsync man thefuck openconnect texlive-most xorg-fonts ssss screen nginx wireguard-tools traceroute unzip
 
 # GUI Programms
-yay -Sy --needed firefox thunderbird keepassxc nextcloud-client torbrowser-launcher okular spectacle libreoffice-fresh obsidian elisa gimp kcalc kate inkscape kget ktorrent gwenview
+yay -Sy --needed firefox thunderbird keepassxc nextcloud-client torbrowser-launcher okular spectacle libreoffice-fresh obsidian elisa gimp kcalc kate inkscape kget ktorrent gwenview krdc
+
+# Remote desktop
+yay -Sy --needed libvncserver libssh freerdp keditbookmarks
 
 # FS tools
 yay -Sy --needed gparted dosfstools jfsutils f2fs-tools mtools gpart nilfs-utils xfsprogs udftools ntfs-3g reiserfsprogs exfatprogs btrfs-progs
@@ -40,6 +52,11 @@ yay -Sy --needed schildichat-desktop-bin telegram-desktop
 
 # IDEs
 yay -S --needed goland pycharm-professional intellij-idea-ultimate-edition code goland-jre intellij-idea-ultimate-edition-jre
+
+# Locales
+sudo echo "de_DE.UTF-8 UTF-8" >> /etc/locale.gen
+sudo echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen 
+sudo localedef -i de_DE -f UTF-8 en_DE.UTF-8
 
 # change user shell to fish
 sudo chsh -s /usr/bin/fish omikron
